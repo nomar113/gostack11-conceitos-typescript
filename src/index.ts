c984de3996815d7434ec279a3bedb2cf9bf634e7
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-04-19 13:05:14 -0300

    Quando adicionar tipos

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import express, { response } from 'express';
-
+import express from 'express';
+import { helloWorld } from './routes';
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World!' });
-});
+app.get('/', helloWorld);
 
 app.listen(3333); 
