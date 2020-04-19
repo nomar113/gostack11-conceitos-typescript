673fe1783e9e78473cb73a544241c21c42d9a658
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-04-19 15:23:23 -0300

    Tipando objetos e vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+    const user = createUser({
+        email: 'abc',
+        password: '123',
+        techs: ['Node.js', 'C++', {title: 'JavaScript', experience: 100}]
+    });
+
     return response.json({ message: 'Hello World!' });
 };
